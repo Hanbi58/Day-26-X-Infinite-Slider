402f66580c1ded2ac2e7c204500d2b3526bb8188
Hanbi58 <hanxiaonan2012@gmail.com>
2022-09-25 23:34:16 -0700

    Infinite Slider

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert('1')
+const sliderContainer = document.querySelector(".sliderContainer");
+const sliders = document.querySelectorAll(".slider");
+const nxtBtn = document.querySelector("#nxtBtn");
+let counter = 1;
+const size = 200;
+
+sliderContainer.style.transform = `translateX(-${size * counter}px)`;
+
+nxtBtn.addEventListener("click", () => {
+  if (counter === 5) {
+    return;
+  }
+  sliderContainer.style.transition = "transform 0.2s ease";
+  counter++;
+  sliderContainer.style.transform = `translateX(-${size * counter}px)`;
+  console.log(counter);
+});
+
+sliderContainer.addEventListener("transitionend", () => {
+  if (counter === 5) {
+    sliderContainer.style.transition = "none";
+    counter = 1;
+    sliderContainer.style.transform = `translateX(-${size * counter}px)`;
+  }
+});
